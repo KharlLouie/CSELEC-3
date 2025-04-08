02068e7eb7b3738ac070912030f278b2c5aa757b
KharlLouie <almonguerrakharllouie@gmail.com>
2025-04-08 10:24:15 +0800

    delete mistake push

--- CSELEC-3-main/Distributed Student Performance Analytics System/Distributed Analytics System/routes/students/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# This creates the blueprint
-student_bp = Blueprint('student_bp', __name__)  # Changed name for clarity
-
-# These imports MUST come after blueprint creation
-from . import performance, subjects, at_risk
--- CSELEC-3-main/Distributed Student Performance Analytics System/Distributed Analytics System/routes/subjects/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# This creates the blueprint
-subject_bp = Blueprint('subject_bp', __name__)  # Changed name for clarity
-
-# These imports MUST come after blueprint creation
-from . import analytics
